db2bb60827b5f3a66caa3fba9eba6d9f4dc3ed8d
nubmaster-69 <chihieunguyen9a2@gmail.com>
2021-03-07 09:11:09 +0700

    Digital Clock

---
+++ Digital Clock/app.js
@@ -0,0 +1,34 @@
+const months = ["Dec", "Jan", "Feb", "Mar", "Apr", "May", "June",
+    "July", "Aug", "Sep", "Oct", "Nov"
+];
+
+const dates = ["sun", "mon", "tue", "wed", "thu",
+    "fri", "sat"
+];
+
+const curDate = new Date();
+
+const curHour = document.querySelector('#hour');
+const curMinute = document.querySelector('#minute');
+const curSecond = document.querySelector('#second');
+
+const curDateOfMonth = document.querySelector('#date-of-month');
+const curMonth = document.querySelector('#month');
+const curYear = document.querySelector('#year');
+
+setInterval(() => {
+    const curTime = new Date();
+    curHour.textContent = (curTime.getHours() > 9 ? curTime.getHours() : '0' + curTime.getHours());
+    curMinute.textContent = (curTime.getMinutes() > 9 ? curTime.getMinutes() : '0' + curTime.getMinutes());
+    curSecond.textContent = (curTime.getSeconds() > 9 ? curTime.getSeconds() : '0' + curTime.getSeconds());
+}, 1);
+
+let upDateCurrentDate = () => {
+    curDateOfMonth.textContent = `${curDate.getDate()}, `;
+    curMonth.textContent = months[curDate.getMonth()];
+    curYear.textContent = curDate.getFullYear();
+
+    document.getElementById(`${dates[curDate.getDay()]}`).style.color = '#fff';
+}
+
+upDateCurrentDate();
